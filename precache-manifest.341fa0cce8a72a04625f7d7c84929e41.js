4ea34788fb6117267697ad1b709eadf0ac2d854a
Bohodar <bodik264478@gmail.com>
2019-07-16 20:05:50 +0300

    Updates

---
+++ precache-manifest.341fa0cce8a72a04625f7d7c84929e41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dd2dc86aec68c71c538b59d68c8fb8",
+    "url": "/frontend-task-3/index.html"
+  },
+  {
+    "revision": "a1c6dcefd716f3759d83",
+    "url": "/frontend-task-3/static/js/2.ae78f552.chunk.js"
+  },
+  {
+    "revision": "bd949ea03299e82f27db",
+    "url": "/frontend-task-3/static/js/main.1f137420.chunk.js"
+  },
+  {
+    "revision": "a42bf52493634124efcc",
+    "url": "/frontend-task-3/static/js/runtime~main.9206b4b2.js"
+  }
+]);
