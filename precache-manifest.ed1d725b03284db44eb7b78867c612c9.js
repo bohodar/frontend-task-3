c9487299a0be5d43d221385bcd9d7cd4aa0dc62d
Bohodar <bodik264478@gmail.com>
2019-07-16 22:59:32 +0300

    Updates

---
+++ precache-manifest.ed1d725b03284db44eb7b78867c612c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5eadf5a3780c47f0eca85472daba6a",
+    "url": "/frontend-task-3/index.html"
+  },
+  {
+    "revision": "d0f5c832a7c603f24e0d",
+    "url": "/frontend-task-3/static/js/2.56632c71.chunk.js"
+  },
+  {
+    "revision": "d60c3a3804c992564b67",
+    "url": "/frontend-task-3/static/js/main.3f65206c.chunk.js"
+  },
+  {
+    "revision": "a42bf52493634124efcc",
+    "url": "/frontend-task-3/static/js/runtime~main.9206b4b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-task-3/precache-manifest.341fa0cce8a72a04625f7d7c84929e41.js"
+  "/frontend-task-3/precache-manifest.ed1d725b03284db44eb7b78867c612c9.js"
 );
 
 self.addEventListener('message', (event) => {
