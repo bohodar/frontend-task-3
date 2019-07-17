5398f184d19173a41e46eb20f0d6496d06792b6c
Bohodar <bodik264478@gmail.com>
2019-07-17 05:03:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-task-3/precache-manifest.ed1d725b03284db44eb7b78867c612c9.js"
+  "/frontend-task-3/precache-manifest.4e546aa3f91fe0accf523d3d4cc5dc4f.js"
 );
 
 self.addEventListener('message', (event) => {
